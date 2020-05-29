af1265ebfbec42b891d053c2147ee7f7d86c693c
Lubov Golikova <lubovgolikova@gmail.com>
2020-05-29 15:01:22 +0300

    add calendar

--- resources/views/datatimes.blade.php
+++ resources/views/datatimes.blade.php
@@ -2,5 +2,22 @@
 @section('content')
     <div class="container services mt-3 mb-5">
         <h1>{{$service->name}}</h1>
+        <h1>{!! $service->description!!}</h1>
+        <h1>{{$service->time}}</h1>
+        <h1>{{$service->price}}</h1>
+
+<div class="calendar">
+        <input id="datepicker" width="276" />
+        <script>
+            $('#datepicker').datepicker({
+                uiLibrary: 'bootstrap4',
+                locale: 'ru-ru',
+            });
+
+        </script>
+</div>
+
     </div>
+
+
 @endsection
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -10,14 +10,23 @@
     <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}" type="image/x-icon">
 
     <link rel="stylesheet" href="{{asset('styles/style.css')}}">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://unpkg.com/gijgo@1.9.13/js/messages/messages.ru-ru.js" type="text/javascript"></script>
+
 </head>
 <body>
 <header id="headerId">
     <nav class="navbar navbar-expand-lg navbar-light bg-light-pink">
         <div class="container">
             <a class="navbar-brand" href="/">ГЛАВНАЯ</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarText">
